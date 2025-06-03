3de6281f259f6e868421b1ef507cecf0cd931217
RuizJul <julrurojas@espol.edu.ec>
2025-06-03 16:09:42 -0500

    initial commit

---
+++ src/proyecto_gestión_contactos/Proyecto_Gestión_Contactos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_gestión_contactos;
+
+/**
+ *
+ * @author Julian
+ */
+public class Proyecto_Gestión_Contactos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
